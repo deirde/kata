28e62d492eed6b7981b7f1d3ac15854171fdfec6
Davide Longo <davidelongo79@gmail.com>
2019-12-17 22:16:55 +0000

    Initial repository.

--- ts/03_infinite_currying/index.spec.js
+++ ts/03_infinite_currying/index.spec.js
@@ -1,12 +1,12 @@
 import { sum } from './index';
 
-test('two positive numbers', () => {
+it('two positive numbers', () => {
   // expect(sum(2, 3)).toBe(5);
   expect(sum(2)(3)).toBe(5);
   // expect(sum(2)(3)(2)(3)).toBe(10);
 });
 
-test('with zeros', () => {
+it('with zeros', () => {
   expect(sum(2, 0)).toBe(2);
   expect(sum(2)(0)).toBe(2);
   expect(sum(0, 3)).toBe(3);
