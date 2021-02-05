9b9786bf07740770aae2b68e944f9b0ab084d7b4
Davide Longo <davidelongo79@gmail.com>
2021-02-05 14:21:47 +0100

    [TS] 09_block_square_in_tic_tac_toe

--- ts/09_block_square_in_tic_tac_toe/index.ts
+++ ts/09_block_square_in_tic_tac_toe/index.ts
@@ -1,3 +1,18 @@
-//https://edabit.com/challenge/kcZfxDswzrwA5kxFX
+/*
+BLOCK THE SQUARE IN TIC TAC TOE
+
+Create a function that returns a number which can block the player from reaching 3 in a row in a game of Tic Tac Toe. The number given as an argument will correspond to a grid of Tic Tac Toe: topleft is 0, topright is 2, bottomleft is 6, and bottomright is 8.
+
+- Create a function that takes two numbers a, b and returns another number.
+
+- This number should be the final one in a line to block the player from winning.
+
+Examples
+blockPlayer(0, 3) ➞ 6
+
+blockPlayer(0, 4) ➞ 8
+
+blockPlayer(3, 5) ➞ 4
+*/
 
 export const blockPlayer = (firstMove: number, secondMove: number): number => (secondMove - firstMove) * 2;
